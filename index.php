66350b5dc61f1fa5cf9239852885b90c99cbfb0d
Ankizle <44819240+Ankizle@users.noreply.github.com>
2020-02-16 15:57:50 -0500

    added scripts and styles

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
   <head>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.core.min.js'></script>
+    <script src='/scripts.js'></script>
+    <link href='/styles.css' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap' rel='stylesheet'>
   </head>
   <table>
