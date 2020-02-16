5a09c0a21e1cdeda485491fcc302655b7f0c120c
Ankizle <44819240+Ankizle@users.noreply.github.com>
2020-02-16 03:02:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,24 @@
 
 <body>
   <head>
-    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+    <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap' rel='stylesheet'>
   </head>
-  <div id='code'>
-    
-  </div>
+  <table>
+    <tr id='top'>
+      <td class='left'>
+        <div id='code'></div>
+      </td>
+      <td class='right'>
+        <div id='passDecry'></div>
+      </td>
+    </tr>
+    <tr id='bottom'>
+      <td class='left'>
+        <div id='binSpam'></div>
+      </td>
+    </tr>
+  </table>
 </body>
   
 </html>
