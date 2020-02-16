910abca5217d50e3ade56be612a884a31a1561bd
Ankizle <44819240+Ankizle@users.noreply.github.com>
2020-02-16 03:04:36 -0500

    added styles

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
     <script src='/scripts.js'></script>
     <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap' rel='stylesheet'>
+    <link href='/styles.css' rel='stylesheet'></link>
   </head>
   <table>
     <tr id='top'>
